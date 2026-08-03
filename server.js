9617b1db4ee3fc807bffd96a6cb5e9e71fa38c04
kese093-rgb <kese093@gmail.com>
2026-08-03 20:51:39 +0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log('Server running on port ' + PORT);
+});
